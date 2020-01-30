eacddd8e7c0ab062c4a23a61289ce2c452dfc3cc
Michael VanSleen <mike.vansleen@gmail.com>
2020-01-29 17:13:26 -0700

    Can update and delete posts

--- routes/posts.js
+++ routes/posts.js
@@ -25,4 +25,37 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Get a specific post
+router.get("/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// Delete a post
+router.delete("/:postId", async (req, res) => {
+  try {
+    const deletedPost = await Post.remove({ _id: req.params.postId });
+    res.json({ message: `Deleted post with ID ${req.params.postId}` });
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// Update a post
+router.patch("/:postId", async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedPost);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
 module.exports = router;
